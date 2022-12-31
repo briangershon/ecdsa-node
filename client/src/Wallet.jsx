81cdfd666b7693b2f609def714e0682f3145fa8f
Brian Gershon <briangershon@users.noreply.github.com>
2022-12-31 12:32:30 -0800

    Get balances via private key implementation.
    Generate example keys to play with.

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,9 +1,20 @@
 import server from "./server";
+import { getPublicKey } from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({
+  privateKey,
+  setPrivateKey,
+  address,
+  setAddress,
+  balance,
+  setBalance,
+}) {
   async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+    const address = toHex(getPublicKey(privateKey));
+    setAddress(address.slice(-20));
     if (address) {
       const {
         data: { balance },
@@ -19,9 +30,15 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input
+          placeholder="Type your private key, for example 04ddef... (no leading 0x)"
+          value={privateKey}
+          onChange={onChange}
+        ></input>
       </label>
+
+      <div>Address: {address}</div>
 
       <div className="balance">Balance: {balance}</div>
     </div>
--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "048b84a669a8215ec00ceb242da21e4851ab728ba315705a2bcf97bf772228d2e400f99bad63e5d0981590e8ce2d5068de6e838ec411fa2ab9ace2895249e2ce1e": 100,
+  "04ddefb40dc7234c2dfa5cae4f2a0ddb556cb816da4094df4ee84769bc3aa9667555a28a82688554d3301811c053d707b008b64dad7c9b6a7fead4073d089eb462": 50,
+  "049f06b051f19e4d40a1d0f42d4fe5cfa507dc1c81fb8f222e64ff1c6417cf98c93aa52311844b77e3f452dfc7e514be08009a7bc5bc863279754fb14d3676ab7c": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
