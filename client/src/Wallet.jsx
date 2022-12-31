c4be46fc150b8976ab77d81b913de054374e7f71
Brian Gershon <briangershon@users.noreply.github.com>
2022-12-31 12:40:36 -0800

    Add error handling

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,6 +1,7 @@
 import server from "./server";
 import { getPublicKey } from "ethereum-cryptography/secp256k1";
 import { toHex } from "ethereum-cryptography/utils";
+import { useState } from "react";
 
 function Wallet({
   privateKey,
@@ -13,17 +14,27 @@ function Wallet({
   async function onChange(evt) {
     const privateKey = evt.target.value;
     setPrivateKey(privateKey);
-    const address = toHex(getPublicKey(privateKey));
-    setAddress(address.slice(-20));
-    if (address) {
-      const {
-        data: { balance },
-      } = await server.get(`balance/${address}`);
-      setBalance(balance);
-    } else {
+    setErrorMessage("");
+    try {
+      const address = toHex(getPublicKey(privateKey));
+      setAddress(address.slice(-20));
+      if (address) {
+        const {
+          data: { balance },
+        } = await server.get(`balance/${address}`);
+        setBalance(balance);
+      } else {
+        setBalance(0);
+        setAddress("");
+      }
+    } catch (e) {
+      setErrorMessage(e.message);
       setBalance(0);
+      setAddress("");
     }
   }
+
+  const [errorMessage, setErrorMessage] = useState("");
 
   return (
     <div className="container wallet">
@@ -37,6 +48,12 @@ function Wallet({
           onChange={onChange}
         ></input>
       </label>
+
+      {errorMessage && (
+        <div style={{ color: "red", marginBottom: "10px" }}>
+          Error: {errorMessage}
+        </div>
+      )}
 
       <div>Address: {address}</div>
 
